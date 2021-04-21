018b96df0f91a3ebfdf03891d3943846db977638
Armando Berlangae <Jose.berlangam@udem.edu>
2021-04-21 00:34:51 -0500

    filtros funcionando

--- web/views.py
+++ web/views.py
@@ -12,44 +12,24 @@ from django.shortcuts import render
 
 def bienvenida(request):
     incidentes = list(Incidente.objects.all())
+    copia = []
+    for i in incidentes:
+        copia.append(i)
 
     if request.method == "POST":
         form = InfoFiltros(request.POST)
         if form.is_valid:
             fecha_forms = str(form["fecha"].value())
-            nom_empleado = form["nombre_empleado"].value()
+            nom_empleado = str(form["nombre_empleado"].value())
             descripcion_forms = str(form["descripcion_incidente"].value())
             estatus = str(form["estatus"].value())
 
+            for i in incidentes:
+                if not ((fecha_forms == str(i.fecha) or not fecha_forms) and nom_empleado in str(i.reporto_persona) and
+                descripcion_forms in str(i.descripcion) and (estatus == str(i.estatus) or not estatus)):
+                    copia.remove(i) 
     else:
         form = InfoFiltros(request.POST)
 
-    incidentes = devolver_lista_filtrada(incidentes, fecha_forms, nom_empleado, descripcion_forms, estatus)
-    return render(request, "bienvenida.html", {"total": len(incidentes), "incidentes": incidentes, "form": form})
-
-
-def is_not_blank(s):
-    if s and s.strip():
-        return True
-    return False
-
-
-def devolver_lista_filtrada(incidentes: list, fecha_forms, nom_empleado, descripcion_forms, estatus):
-    filtrados = []
-    for i in incidentes:
-        fecha = str(i.fecha)
-        if (is_not_blank(fecha) and fecha == fecha_forms):
-            filtrados.append(i)
-
-        rep_persona = str(i.reporto_persona)
-        if (is_not_blank(rep_persona) and (i not in filtrados) and rep_persona.__contains__(nom_empleado)):
-            filtrados.append(i)
-
-        descripcion = str(i.descripcion)
-        if (is_not_blank(descripcion) and (i not in filtrados) and descripcion.__contains__(descripcion_forms)):
-            filtrados.append(i)
-
-        if (not estatus or ((i not in filtrados) and estatus == str(i.estatus))):
-            filtrados.append(i)
-
-    return filtrados
+    copia = copia if copia else incidentes
+    return render(request, "bienvenida.html", {"total": len(copia), "incidentes": copia, "form": form})
